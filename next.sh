013e201095482959d1bfbf883552bacbbc5a50dc
Yann <yann.buydens@gmail.com>
2015-09-29 23:51:55 +0900

    mvp next.sh file

--- next.sh
+++ next.sh
@@ -4,7 +4,12 @@ IN_MASTER_BRANCH=$(git status | head -n 1 | grep 'On branch master')
 
 if [ -n "$IN_MASTER_BRANCH" ];
 then
-  echo 'do nothing';
+  sh start.sh
 else
-  git checkout master
+  current_commit_hash=$(sh current_commit_hash)
+  current_commit_number=$(cat ~/tmp/git_extras_hashes | grep -n "$current_commit_hash" | awk -F':' '{print $1}')
+  next_commit_number = $current_commit_number - 1
+  next_commit_hash =  $(sed -e $next_commit_number'q;d' ~/tmp/git_extras_hashes)
+  git checkout $next_commit_hash
+  echo 'You are in the next commit'
 fi
