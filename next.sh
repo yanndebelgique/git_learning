b9f0026e45a2ea4c6acf8de7ad83b83c7dd99260
Yann <yann.buydens@gmail.com>
2015-09-30 00:05:13 +0900

    made mvp work

--- next.sh
+++ next.sh
@@ -6,10 +6,10 @@ if [ -n "$IN_MASTER_BRANCH" ];
 then
   sh start.sh
 else
-  current_commit_hash=$(sh current_commit_hash)
-  current_commit_number=$(cat ~/tmp/git_extras_hashes | grep -n "$current_commit_hash" | awk -F':' '{print $1}')
-  next_commit_number = $current_commit_number - 1
-  next_commit_hash =  $(sed -e $next_commit_number'q;d' ~/tmp/git_extras_hashes)
+  current_commit_hash=$(sh ~/Desktop/p/personal/github_projects/git-extras-yann/current_commit_hash)
+  current_commit_number=$(cat ~/tmp/git_extras_hashes | grep -n $current_commit_hash | awk -F':' '{print $1}')
+  next_commit_number=`expr $current_commit_number - 1`
+  next_commit_hash=$(sed -e $next_commit_number'q;d' ~/tmp/git_extras_hashes)
   git checkout $next_commit_hash
   echo 'You are in the next commit'
 fi
--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 git log | grep '^commit ' | awk '{ print $2, " "}' > ~/tmp/git_extras_hashes
 echo "hi there! lets explore this git repo. first things first.\n"
 echo "Lets go to the first commit."
-sh first_commit | tail -n 1
+sh ~/Desktop/p/personal/github_projects/git-extras-yann/first_commit | tail -n 1
 echo 'please type "git_learn next" to go to the next commit\n'
 echo 'please type "git_learn previous" to go back to the previous commit \n'
